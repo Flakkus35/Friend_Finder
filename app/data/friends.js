1a662f7d2217d706d6ec34079363e63aaac70d69
Devon Wood <wooddv35@gmail.com>
2017-09-27 16:03:44 -0400

    finished main functionality

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+	{
+		name: 'Billy',
+		photo: 'http://www.worldwaterforum8.org/sites/default/files/Picture%20Hassan.jpg',
+		scores: [
+			5,
+			3,
+			2,
+			5,
+			4,
+			1,
+			3,
+			2,
+			1,
+			5
+		]
+	},
+	{
+		name: 'Nick',
+		photo: 'https://imgix.ranker.com/node_img/84/1675916/original/nick-cannon-writers-photo-1?w=650&q=50&fm=jpg&fit=crop&crop=faces',
+		scores: [
+			4,
+			2,
+			1,
+			4,
+			5,
+			3,
+			2,
+			2,
+			5,
+			4
+		]
+	},
+	{
+		name: 'Chris',
+		photo: 'https://peopledotcom.files.wordpress.com/2017/08/peter-kraus.jpg?crop=379px%2C100px%2C1416px%2C947px&resize=728%2C486',
+		scores: [
+			5,
+			3,
+			2,
+			3,
+			5,
+			3,
+			5,
+			4,
+			1,
+			3
+		]
+	},
+	{
+		name: 'George',
+		photo: 'https://peopledotcom.files.wordpress.com/2017/09/gettyimages-841823794.jpg?crop=2px%2C0px%2C2696px%2C1799.8021978022px&resize=728%2C486',
+		scores: [
+			4,
+			5,
+			1,
+			2,
+			4,
+			5,
+			1,
+			3,
+			1,
+			3
+		]
+	},
+	{
+		name: 'Emma',
+		photo: 'https://i.pinimg.com/736x/23/06/52/23065225081d2f6c0f5f6d9bf634e372--pretty-people-beautiful-people.jpg',
+		scores: [
+			3,
+			2,
+			3,
+			4,
+			5,
+			1,
+			2,
+			4,
+			5,
+			1
+		]
+	}
+]
+
+module.exports = friends;
